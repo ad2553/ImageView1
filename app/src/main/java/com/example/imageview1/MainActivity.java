5170be632308647aeb99a3013d08119b66c62633
ad2553 <ad2553@bs.amalnet.k12.il>
2024-01-02 09:10:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/imageview1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.imageview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import java.util.Random;
+public class MainActivity extends AppCompatActivity {   
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+
+    }
+}
