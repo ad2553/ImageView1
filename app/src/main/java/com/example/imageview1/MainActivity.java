fc2a54ca702a5848f8c07d0eab45f9f066b45388
ad2553 <ad2553@bs.amalnet.k12.il>
2024-01-02 09:40:41 +0200

    phase 2 complete

--- app/src/main/java/com/example/imageview1/MainActivity.java
+++ app/src/main/java/com/example/imageview1/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
 import java.util.Random;
 public class MainActivity extends AppCompatActivity {
+ImageView iv;
+
+Butto btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+        btn = findViewById(R.id.btn);
+
     }
 
     public void click(View view) {
